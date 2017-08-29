462630579f56b5e9bbebbfa0d54fa189a3f599d3
allen <liuxiangxi12@163.com>
2017-08-29 14:31:13 +0800

    设置redis启动脚本

--- script/redis-trib-cluster/redis-trib/entrypoint.sh
+++ script/redis-trib-cluster/redis-trib/entrypoint.sh
@@ -1,6 +1,23 @@
 #! /usr/bin/env bash
 
 /usr/src/redis/redis-3.2.8/src/redis-trib.rb create --replicas --yes ${NODE_SUM} ${REDIS_NODE}
-##redis-server /usr/local/etc/redis/redis.conf
-##redis-server
+
+PASSWORD="hello123"
+
+OLD_IFS="$IFS"
+IFS=" "
+REDIS_ARR=($REDIS_NODE)
+
+for item in ${REDIS_ARR[@]}
+do
+	IFS=":"
+	node=($item)
+	ip=${node[0]}
+	port=${node[1]}
+	## 启动后设置集群密码
+	echo "config set masterauth ${PASSWORD}" | redis-cli -p ${port} -h ${ip}
+	echo "config set requirepass ${PASSWORD}" | redis-cli -p ${port} -h ${ip}
+done
+
+IFS="$OLD_IFS"
 /usr/bin/env bash
