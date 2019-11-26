8afcd9a53f3655bdafad1cfeb42049377933cf19
allen <liuxiangxi@ly78.com.cn>
2019-11-26 09:41:01 +0800

    update

--- dockerfile/laravel/build_docker_image.sh
+++ dockerfile/laravel/build_docker_image.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash 
 
-docker build --rm -t laravel:0.1.6 .
+docker build --rm -t laravel:0.1.7 .
 docker images
---
+++ dockerfile/php5.5-swoole/build_docker_image.sh
@@ -0,0 +1,4 @@
+#! /usr/bin/env bash   
+
+docker build --rm -t php-swoole:5.5-cli .
+docker images
